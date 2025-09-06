15f18299931a1717299825d910cec05d01ac103b
thiagoptg <thiagoalexandre.pdbd254@pdbomdespacho.com.br>
2025-09-06 20:50:44 -0300

    Add files via upload

---
+++ modulo 2/aula2_questaoX.py
@@ -0,0 +1,34 @@
+#questão 1
+a=5
+b=2
+print (type(a))
+print (type(b))
+c=a/b
+print (c)
+print (type(c))
+#Questão 2
+a= "o resultado é"
+b=10
+c=3.5
+soma=b+c
+print (a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(soma)
+print(type(soma))
+#questão 3
+v1=10
+v2=20
+print (v1+10)
+print(v2-10)
+#Questão 4
+juros = 1.01
+saldo = 500.0
+saldo = saldo * juros
+saldo = saldo * juros
+saldo = saldo * juros
+print("Após 3 meses meu novo saldo é")
+print(saldo)
